89c6a0f23e7553abb9b17d23d031dba36ca22e4b
Cristian Chirac <cristian.chirac@uipath.com>
2021-05-17 12:44:27 +0300

    feat(todos): Emphasize search pattern in results

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -2,19 +2,22 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import {
-    FormsModule,
-    ReactiveFormsModule,
+  FormsModule,
+  ReactiveFormsModule,
 } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 
 import { TodosSearchFilterPipe } from './pipes/todos-search-filter.pipe';
 import { TodosComponent } from './todos.component';
+import { EmphasizePatternPipe } from './pipes/emphasize-pattern.pipe';
+import { UiNgLetModule } from '@uipath/angular/directives/ui-ng-let';
 
 @NgModule({
   declarations: [
     TodosComponent,
     TodosSearchFilterPipe,
+    EmphasizePatternPipe,
   ],
   imports: [
     CommonModule,
@@ -23,6 +26,7 @@ import { TodosComponent } from './todos.component';
     ReactiveFormsModule,
     MatFormFieldModule,
     HttpClientModule,
+    UiNgLetModule,
   ],
   exports: [
     TodosComponent,
