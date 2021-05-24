5c6fc16d87ba320ca26a5628dbd08835f339a7c3
Cristian Chirac <cristian.chirac@uipath.com>
2021-05-24 17:19:16 +0300

    feat(todos): Add arrow-up/down autocomplete navigation

---
+++ src/app/common/constants/keyboard_utils.ts
@@ -0,0 +1,6 @@
+export const KEY_NAMES = {
+  ARROW_DOWN: 'ArrowDown',
+  ARROW_UP: 'ArrowUp',
+  ENTER: 'Enter',
+  ESCAPE: 'Escape',
+} as const;
--- src/app/todos/todos-autocomplete/todos.component.ts
+++ src/app/todos/todos-autocomplete/todos.component.ts
@@ -6,14 +6,21 @@ import {
     EventEmitter,
     HostListener,
     Input,
+    OnDestroy,
+    OnInit,
     Output,
+    QueryList,
     ViewChild,
+    ViewChildren,
 } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 import {
     BehaviorSubject,
+    combineLatest,
     Observable,
+    Subject,
+    Subscription,
 } from 'rxjs';
 import {
     catchError,
@@ -23,6 +30,7 @@ import {
     startWith,
     switchMap,
     tap,
+    withLatestFrom,
 } from 'rxjs/operators';
 import { KEY_NAMES } from 'src/app/common/constants/keyboard_utils';
 
@@ -35,18 +43,19 @@ import { TodosService } from './todos.service';
   styleUrls: ['./todos.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TodosComponent implements AfterViewInit {
+export class TodosComponent implements AfterViewInit, OnDestroy {
   @Input() sourceUrls: string[] = [];
 
   @Output() itemSelected = new EventEmitter<ITodo>();
   @Output() close = new EventEmitter<void>();
 
   @ViewChild('searchBarInput', {static: true}) searchBarInput!: ElementRef;
+  @ViewChildren('todos') todosListViewElements!: QueryList<any>;
 
   public search = new FormControl('');
   public errorMessageAction$: Observable<string>;
-
   public searchText$ = this.search.valueChanges;
+  public focusSuggestionIndexAction$: Observable<number>;
 
   public results$ = this.searchText$.pipe(
     startWith(''),
@@ -62,12 +71,67 @@ export class TodosComponent implements AfterViewInit {
 
   private _errorMessage$ = new BehaviorSubject<string>('');
 
+  private _focusSuggestionIndex$ = new BehaviorSubject<number>(-1);
+
+  private _focusSuggestionArrowMove$ = new Subject<
+    typeof KEY_NAMES.ARROW_UP |
+    typeof KEY_NAMES.ARROW_DOWN
+  >();
+  private _focusSuggestionArrowMoveSubscription: Subscription;
+  private _focusSuggestionScrollToSubscription!: Subscription;
+
+  private _focusSuggestionEnter$ = new Subject<void>();
+  private _focusSuggestionEnterSubscription!: Subscription;
+
   constructor(private _todosService: TodosService) {
     this.errorMessageAction$ = this._errorMessage$.asObservable();
+    this.focusSuggestionIndexAction$ = this._focusSuggestionIndex$.asObservable();
+
+    this._focusSuggestionArrowMoveSubscription = this._focusSuggestionArrowMove$.pipe(
+      withLatestFrom(
+        this.results$.pipe(map(results => results.length)),
+        this._focusSuggestionIndex$,
+      ),
+      tap(([arrowKey, resultsLength, currFocusIndex]) => {
+        const direction = arrowKey === KEY_NAMES.ARROW_DOWN ? 1 : -1;
+        const newIndex = currFocusIndex + direction;
+
+        if (newIndex < 0 || newIndex >= resultsLength) {
+          return;
+        }
+
+        this._focusSuggestionIndex$.next(newIndex);
+      }),
+    ).subscribe();
+
+    this._focusSuggestionEnterSubscription = this._focusSuggestionEnter$.pipe(
+      withLatestFrom(
+        this.results$,
+        this._focusSuggestionIndex$
+      ),
+      tap(([_, todos, selectedTodoIndex]) => this.itemSelected.emit(todos[selectedTodoIndex])),
+    ).subscribe();
   }
 
   ngAfterViewInit(): void {
     this.searchBarInput.nativeElement.focus();
+    this._focusSuggestionScrollToSubscription = this._focusSuggestionIndex$.pipe(
+      withLatestFrom(this.todosListViewElements.changes),
+      tap(([suggestionIndex, resultsElems]) => {
+        const suggestionElem = resultsElems.toArray()[suggestionIndex];
+        suggestionElem.nativeElement.scrollIntoView({
+          behavior: "smooth",
+          block: "end",
+          inline: "nearest"
+        });
+      }),
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this._focusSuggestionArrowMoveSubscription.unsubscribe();
+    this._focusSuggestionEnterSubscription.unsubscribe();
+    this._focusSuggestionScrollToSubscription.unsubscribe();
   }
 
   public todoSelectedHandler(item: ITodo) {
@@ -76,8 +140,17 @@ export class TodosComponent implements AfterViewInit {
 
   @HostListener('window:keyup', ['$event'])
   private keyEvent(event: KeyboardEvent) {
-    if (event.key === KEY_NAMES.ESCAPE) {
-      this.close.emit();
+    switch (event.key) {
+      case KEY_NAMES.ESCAPE:
+        this.close.emit();
+        break;
+      case KEY_NAMES.ARROW_DOWN:
+      case KEY_NAMES.ARROW_UP:
+        this._focusSuggestionArrowMove$.next(event.key);
+        break;
+      case KEY_NAMES.ENTER:
+        this._focusSuggestionEnter$.next();
+        break;
     }
   }
 
